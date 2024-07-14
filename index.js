5e0e27378f248da774aa64f1b3817d39fe5e8cd3
Isuu1 <jakub.babelek@gmail.com>
2024-07-14 20:28:31 +0100

    Revert changes

--- index.js
+++ index.js
@@ -8,12 +8,8 @@ const userInput = document.getElementById("user-input");
 //add element to list
 function addToList() {
   const newElement = document.createElement("li");
-  //Add delete button
-  const deleteBtn = document.createElement("button");
-  deleteBtn.textContent = "x";
   //Take user input value
   newElement.textContent = userInput.value;
-  newElement.appendChild(deleteBtn);
   list.appendChild(newElement);
   //Reset user input after adding element to list
   userInput.value = "";
