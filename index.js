1782eff9b74b74799b63446c99ff56dfbf6ae82b
Isuu1 <jakub.babelek@gmail.com>
2024-07-14 19:00:17 +0100

    Target DOM elements in js file

---
+++ index.js
@@ -0,0 +1,6 @@
+//Target list element
+const list = document.getElementById("list");
+//Target user input
+const userInput = document.getElementById("user-input");
+console.log(list);
+console.log(userInput);
