ad307fe628316c6dcae174c113b9d52a2a17f8e4
Isuu1 <jakub.babelek@gmail.com>
2024-07-14 19:36:36 +0100

    Created a function to add item to list

--- README.md
+++ README.md
@@ -6,6 +6,6 @@
 - [x] Add style and script
 - [x] Add HTML elements for user input, button and list
 - [x] Target DOM elements in javascript file
-- [ ] Create a function to add item
+- [x] Create a function to add item
 - [ ] Create a function to remove item
 - [ ] Add styling
--- index.js
+++ index.js
@@ -2,5 +2,15 @@
 const list = document.getElementById("list");
 //Target user input
 const userInput = document.getElementById("user-input");
-console.log(list);
-console.log(userInput);
+
+//create new element
+//add user input as text
+//add element to list
+function addToList() {
+  const newElement = document.createElement("li");
+  //Take user input value
+  newElement.textContent = userInput.value;
+  list.appendChild(newElement);
+  //Reset user input after adding element to list
+  userInput.value = "";
+}
